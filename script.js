de4421433b97e8a950ba8d27168765bf10e08732
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-06-01 19:28:00 +0200

    Add HRV data for April and May

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function movingStandardDeviation(arr, windowSize) {
 let allData = [];
 
 // Funzione per leggere il CSV
-Papa.parse('hrv_data.csv', {
+Papa.parse('https://raw.githubusercontent.com/sasadangelo/hrv/main/hrv_data.csv', {
     download: true,
     header: true,
     complete: function(results) {
@@ -43,7 +43,6 @@ Papa.parse('hrv_data.csv', {
 });
 
 function setDefaultDates() {
-    console.log("1111111")
     const today = new Date().toISOString().split('T')[0];
     const pastDate = new Date();
     pastDate.setDate(pastDate.getDate() - 30);
@@ -69,11 +68,16 @@ function processData() {
 
     const dates = filteredData.map(row => row.date);
     const rmssdValues = filteredData.map(row => parseFloat(row.rmssd));
+    console.log("RMSSD: ", rmssdValues)
     const lnRmssdValues = rmssdValues.map(value => Math.log(value));
+    console.log("ln(RMSSD): ", lnRmssdValues)
 
     const movingAvg7 = movingAverage(lnRmssdValues, 7);
     const movingAvg30 = movingAverage(lnRmssdValues, 30);
     const movingStdDev30 = movingStandardDeviation(lnRmssdValues, 30);
+    console.log("Media mobile 7gg: ", movingAvg7)
+    console.log("Media mobile 30gg: ", movingAvg30)
+    console.log("Dev Standard 30gg: ", movingStdDev30)
 
     const upperLimits = movingAvg30.map((avg, index) => {
         const stdDev = movingStdDev30[index];
@@ -90,49 +94,53 @@ function processData() {
     const upperLimit = upperLimits[upperLimits.length - 1];
     const lowerLimit = lowerLimits[lowerLimits.length - 1];
 
-    document.getElementById('currentValue').textContent = currentValue ? currentValue.toFixed(2) : 'N/A';
-    document.getElementById('avg7Day').textContent = avg7Day ? avg7Day.toFixed(2) : 'N/A';
-    document.getElementById('upperLimit').textContent = upperLimit ? upperLimit.toFixed(2) : 'N/A';
-    document.getElementById('lowerLimit').textContent = lowerLimit ? lowerLimit.toFixed(2) : 'N/A';
-
-    createChart(dates, lnRmssdValues, upperLimits, lowerLimits);
+    createChart(dates, lnRmssdValues, upperLimits, lowerLimits, movingAvg7);
 }
 
 let chart;
 
 // Funzione per creare il grafico
-function createChart(dates, lnRmssdValues, upperLimits, lowerLimits) {
+function createChart(dates, lnRmssdValues, upperLimits, lowerLimits, movingAvg7) {
     const ctx = document.getElementById('hrvChart').getContext('2d');
-
-    const backgroundColors = lnRmssdValues.map((value, index) => {
-        const upperLimit = upperLimits[index];
-        const lowerLimit = lowerLimits[index];
-        if (value !== null && upperLimit !== null && lowerLimit !== null) {
-            if (value > upperLimit) {
-                return 'green';
-            } else if (value < lowerLimit) {
-                return 'red';
-            } else {
-                return 'yellow';
-            }
-        } else {
-            return 'gray'; // Per valori iniziali dove la media mobile non è calcolabile
-        }
-    });
 
     if (chart) {
         chart.destroy();
     }
 
     chart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: dates,
-            datasets: [{
-                label: 'LN(RMSSD)',
-                data: lnRmssdValues,
-                backgroundColor: backgroundColors
-            }]
+            datasets: [
+                {
+                    label: 'LN(RMSSD)',
+                    data: lnRmssdValues,
+                    borderColor: 'black',
+                    backgroundColor: 'rgba(0,0,0,0.1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Upper Limit',
+                    data: upperLimits,
+                    borderColor: 'green',
+                    borderWidth: 1,
+                    fill: false
+                },
+                {
+                    label: 'Lower Limit',
+                    data: lowerLimits,
+                    borderColor: 'red',
+                    borderWidth: 1,
+                    fill: false
+                },
+                {
+                    label: '7-Day Moving Average',
+                    data: movingAvg7,
+                    borderColor: 'blue',
+                    borderWidth: 1,
+                    fill: false
+                }
+            ]
         },
         options: {
             scales: {
