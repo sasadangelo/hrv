76fed98aa83aa038004cc446c92ebab218d173a6
Salvatore D'Angelo <sasadangelo@gmail.com>
2024-07-15 23:26:48 +0200

    Add automatic update feature

---
+++ fcrest.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from datetime import timedelta, datetime
+
+class FCRestData:
+    def __init__(self, client):
+        self.client = client
+        self.data = None
+
+    def load_csv(self, file_path):
+        print(f"Load FCrest data from the {file_path} CSV file.")
+        self.data = pd.read_csv(file_path, parse_dates=['date'])
+
+    def get_last_date(self):
+        return self.data['date'].max()
+
+    def fetch_new_data(self, start_date, end_date):
+        new_data = []
+        for date in pd.date_range(start=start_date, end=end_date):
+            try:
+                data = self.client.get_rhr_day(date.strftime('%Y-%m-%d'))
+                if (data != None):
+                    if 'allMetrics' in data and 'metricsMap' in data['allMetrics'] and 'WELLNESS_RESTING_HEART_RATE' in data['allMetrics']['metricsMap']:
+                        rhr_list = data['allMetrics']['metricsMap']['WELLNESS_RESTING_HEART_RATE']
+                        for entry in rhr_list:
+                            if entry['calendarDate'] == date.strftime('%Y-%m-%d'):
+                                 new_data.append({'date': date.strftime('%Y-%m-%d'), 'fcr': entry['value']})
+                else:
+                    print(f"No data available for the date {date.strftime('%Y-%m-%d')}")
+            except Exception as e:
+                print(f"Error fetching FC Rest data for {date.strftime('%Y-%m-%d')}: {e}")
+        return new_data
+
+    def update_data(self):
+        last_date = self.get_last_date()
+        today = pd.to_datetime(pd.Timestamp.today().date())
+        if today > last_date:
+            print(f"Update FCR data to the {last_date.strftime('%Y-%m-%d')} date.")
+            new_data = self.fetch_new_data(last_date + timedelta(days=1), today)
+            new_data_df = pd.DataFrame(new_data)
+            self.data = pd.concat([self.data, new_data_df], ignore_index=True)
+        else:
+            print("No new data to update. The dataset is already up-to-date.")
+
+    def save_csv(self, file_path):
+        print(f"Save HRV data to the {file_path} CSV file.")
+        # Ensure 'date' column is in datetime format
+        self.data['date'] = pd.to_datetime(self.data['date'])
+        # Convert 'fcr' column to integer
+        self.data['fcr'] = self.data['fcr'].astype(int)
+        self.data['date'] = self.data['date'].dt.strftime('%Y-%m-%d')
+        #self.data.to_csv(file_path, index=False)
+        self.data.reset_index(drop=True).to_csv(file_path, index=False)
---
+++ update.py
@@ -0,0 +1,75 @@
+import os
+import pandas as pd
+import datetime
+from garminconnect import Garmin
+from hrv import HRVData
+from fcrest import FCRestData
+from tss import TSSData
+from garth.exc import GarthHTTPError
+import requests
+from garminconnect import (
+    Garmin,
+    GarminConnectAuthenticationError,
+    GarminConnectConnectionError,
+    GarminConnectTooManyRequestsError,
+)
+from dotenv import load_dotenv
+
+# Load the EMAIL and PASSWORD from the .env file
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+tokenstore = os.getenv("GARMINTOKENS") or "~/.garminconnect"
+tokenstore_base64 = os.getenv("GARMINTOKENS_BASE64") or "~/.garminconnect_base64"
+
+def main():
+    try:
+        print(f"Trying to login to Garmin Connect using token data from directory '{tokenstore}'...\n")
+
+        client = Garmin()
+        client.login(tokenstore)
+    except (FileNotFoundError, GarthHTTPError, GarminConnectAuthenticationError):
+        print(
+            "Login tokens not present, login with your Garmin Connect credentials to generate them.\n"
+            f"They will be stored in '{tokenstore}' for future use.\n"
+        )
+        try:
+            # Authenticate with Garmin Connect
+            client = Garmin(EMAIL, PASSWORD)
+            client.login()
+            # Save Oauth1 and Oauth2 token files to directory for next login
+            client.garth.dump(tokenstore)
+            print(f"Oauth tokens stored in '{tokenstore}' directory for future use. (first method)\n")
+            # Encode Oauth1 and Oauth2 tokens to base64 string and safe to file for next login (alternative way)
+            token_base64 = garmin.garth.dumps()
+            dir_path = os.path.expanduser(tokenstore_base64)
+            with open(dir_path, "w") as token_file:
+                token_file.write(token_base64)
+            print(f"Oauth tokens encoded as base64 string and saved to '{dir_path}' file for future use. (second method)\n")
+        except (FileNotFoundError, GarthHTTPError, GarminConnectAuthenticationError, requests.exceptions.HTTPError) as err:
+            print(err)
+            return None
+
+    # Load and update HRV data
+    hrv = HRVData(client)
+    hrv.load_csv('data/hrv_data.csv')
+    hrv.update_data()
+    hrv.save_csv('data/hrv_data.csv')
+
+    # Load and update FC Rest data
+    fcrest = FCRestData(client)
+    fcrest.load_csv('data/fcr_data.csv')
+    fcrest.update_data()
+    fcrest.save_csv('data/fcr_data.csv')
+
+    # Load and update TSS data
+    tss = TSSData(client)
+    tss.load_csv('data/tss_data.csv')
+    tss.update_data()
+    tss.save_csv('data/tss_data.csv')
+
+    print("CSV files updated successfully.")
+
+if __name__ == "__main__":
+    main()
